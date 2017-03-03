aa337d8e69543356807478964c9ceac3cb61931b
rytisz <rytiszigmantavicius@gmail.com>
2017-03-04 00:31:31 +0200

    added match and fixed output

--- ethstats.py
+++ ethstats.py
@@ -2,22 +2,24 @@
 
 from optparse import OptionParser
 import pdb
+import time
 
 parser = OptionParser()
 parser.add_option("-n", "--period", dest="PERIOD", default=1,
                   help="average period in seconds, default 1 sec")
 parser.add_option("-i", "--interface", dest="INTERFACE", default="all",
-                  help="interface or interfaces witch statistics is measured")
+                  help="interface witch statistics is measured, default all interfaces")
 
 (options, args) = parser.parse_args()
 
 class ethstats:
 
     def __init__(self):
-        self.period = vars(options)['PERIOD']
-        self.interfaces = vars(options)['INTERFACE']
+        self.period = int( vars(options)['PERIOD'])
+        self.interface = vars(options)['INTERFACE']
+        self.previous= self.__read_stats()
 
-    def read_stats(self):
+    def __read_stats(self):
         d= {}
         l= []
         with open('/proc/net/dev') as f:
@@ -26,14 +28,27 @@ class ethstats:
             for line in f:
                 l= line.split()
                 d[l[0].rstrip(':')]=l[1:]
+        if self.interface == "all":
+            return d
+        
+        return  {self.interface:d[self.interface]}
 
-        return(d)
-#            interface = {line.split(":") for line in lines}
-#            data=[x.split() for x in rez]
-    
+    def get(self):
+        current= self.__read_stats()
+        rez={}
+        for interface in current:
+            rx=(int(current[interface][0]) - int(self.previous[interface][0]))*8/10**6/self.period
+            tx=(int(current[interface][8]) - int(self.previous[interface][8]))*8/10**6/self.period
+            rpps=(int(current[interface][1]) - int(self.previous[interface][1]))/10**3/self.period
+            tpps=(int(current[interface][9]) - int(self.previous[interface][9]))/10**3/self.period
+            rez[interface]=[tx,tpps,rx,rpps]
+        
+        self.previous=current
+        return rez
+
 stats=ethstats()
-#print(stats.read_stats(vars(options)['INTERFACE']))
-print(stats.read_stats())
-
-#    def count_stats():
-
+while True: 
+    time.sleep(stats.period)
+    rez=stats.get()
+    for interface in rez:
+        print("%10s: TX%8.2f Mbps%5.2fKPPS RX%8.2f Mbps%5.2fKPPS "%tuple([interface]+rez[interface]))
